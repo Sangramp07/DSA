3b5bee51d6338ca9924729ad8d8917191a6da7f1
sangram <sangramsinhpawar232@gmail.com>
2026-08-28 00:48:09 +0530

    ld

--- STRING/palindromesubSCount.java
+++ STRING/palindromesubSCount.java
@@ -2,4 +2,24 @@ package STRING;
 
 public class palindromesubSCount {
     
+
+    public int countSubstring(String s){
+        int n=s.length();
+        int count=0;
+        for(int i=0;i<n;i++){
+            count+=expand(s, i, i);
+            count+=expand(s, i, i+1);
+        }
+        return count;
+    }
+    private int expand(String  s,int left,int right){
+        int count=0;
+        //checking palindrome
+        while (left>=0 && right<s.length() && s.charAt(left)==s.charAt(right)) {
+            count ++;
+            left--;
+            right++;
+            
+        }return count;
+    }
 }
