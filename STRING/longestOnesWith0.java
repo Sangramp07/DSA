27da6cc2e0a9ecd9ffe6cb1d6ecef483c105670c
sangram <sangramsinhpawar232@gmail.com>
2026-08-31 22:45:02 +0530

    ld

---
+++ STRING/longestOnesWith0.java
@@ -0,0 +1,22 @@
+package STRING;
+
+public class longestOnesWith0 {
+    public int longestOnes(int[] nums, int k) {
+        int left=0;
+        int zerocount=0;
+        int n=nums.length;
+
+        for(int right=0;right<n;right++){
+            if(nums[right]==0){
+                zerocount++;
+            }
+            if(zerocount>k){
+                if(nums[left]==0){
+                    zerocount--;
+                }
+                left++;
+            }
+        }
+        return n-left;
+    }
+}
