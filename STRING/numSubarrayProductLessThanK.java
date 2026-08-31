2a58b115ee97d255bc0349645c24f3d8c298e165
sangram <sangramsinhpawar232@gmail.com>
2026-08-31 23:21:14 +0530

    ld

---
+++ STRING/numSubarrayProductLessThanK.java
@@ -0,0 +1,27 @@
+package STRING;
+
+class Solution {
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        int n=nums.length;
+
+        if(k<=1){
+            return 0;
+        }
+        int left=0;
+        int prod=1;
+        int count=0;
+        for(int right=0;right<n;right++){
+            prod*=nums[left];
+
+            while(prod>=k){
+                prod/=nums[left];
+                left++;
+            }
+            count+=right-left+1;
+            
+
+        }
+
+        return count;
+    }
+}
