0c133fa4ef31d88760419c7917bb0362d41fd27f
sangram <sangramsinhpawar232@gmail.com>
2026-09-01 10:29:41 +0530

    l

--- STRING/subarraysWithKDistinct.java
+++ STRING/subarraysWithKDistinct.java
@@ -14,7 +14,7 @@ public class subarraysWithKDistinct {
         for(int right=0; right<nums.length; right++){
             map.put(nums[right],map.getOrDefault(nums[right],0)+1);
 
-            while(map.size()>2){
+            while(map.size()>k){
                 map.put(nums[left],map.get(nums[left])-1);
                 if(map.get(nums[left])==0){
                     map.remove(nums[left]);
