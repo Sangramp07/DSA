26e17fb40f2e381a3f06a14c86683f3ff1eeebac
sangram <sangramsinhpawar232@gmail.com>
2026-09-02 15:32:29 +0530

    l

---
+++ STRING/longestCommonPrefix.java
@@ -0,0 +1,22 @@
+package STRING;
+
+import java.util.Arrays;
+
+public class longestCommonPrefix {
+    public String longestCommonPrefix(String[] strs) {
+        Arrays.sort(strs);
+        String s = "";
+        int i = 0;
+        int length = strs.length;
+
+        while (i < strs[0].length()) {
+            if (strs[0].charAt(i) == strs[length - 1].charAt(i))
+                s += strs[0].charAt(i);
+            else
+                break;
+            i++;
+        }
+
+        return s;
+    }
+}
