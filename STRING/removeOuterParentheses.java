a72c4c2b9934d41106f7ed216692f060c5300706
sangram <sangramsinhpawar232@gmail.com>
2026-09-03 11:17:25 +0530

    l

---
+++ STRING/removeOuterParentheses.java
@@ -0,0 +1,20 @@
+package STRING;
+
+public class removeOuterParentheses {
+    public String removeOuterParentheses(String s) {
+        int cnt=0;
+        String  ans=new String();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)==')'){
+                cnt--;
+            }
+            if(cnt!=0){
+                ans+=s.charAt(i);
+            }
+            if(s.charAt(i)=='('){
+                cnt++;
+            }
+        }return ans;
+    }
+    
+}
