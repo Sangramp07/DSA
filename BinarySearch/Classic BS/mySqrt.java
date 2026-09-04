6e61760a8af3ef8d16210b55d18e95e5a807994b
sangram <sangramsinhpawar232@gmail.com>
2026-09-04 11:51:48 +0530

    l

--- BinarySearch/Classic BS/mySqrt.java
+++ BinarySearch/Classic BS/mySqrt.java
@@ -1,5 +1,24 @@
-package Classic BS;
+
 
 public class mySqrt {
+    public int mySqrt(int x){
+        int left=1,right=x/2;
+
+        while(left<=right){
+            int mid= left+(right-left)/2;
+
+            long square = (long) mid * mid;
+
+            if (square == x) {
+                return mid; // perfect square
+            } else if (square < x) {
+                // right = mid; // store as possible answer
+                left = mid + 1; // look for bigger
+            } else {
+                right = mid - 1; // look for smaller
+            }
+        }
+        return right;
     
+    }
 }
