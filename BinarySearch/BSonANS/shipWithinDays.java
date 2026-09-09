2ccdcc5b630124cc2160733bf3bb3ef977229f83
sangram <sangramsinhpawar232@gmail.com>
2026-09-09 10:14:46 +0530

    l

---
+++ BinarySearch/BSonANS/shipWithinDays.java
@@ -0,0 +1,37 @@
+package BSonANS;
+
+public class shipWithinDays {
+   public int shipWithinDays(int[] weights, int days) {
+        int low=0;
+        int high=0;
+        for(int w:weights){
+            low=Math.max(low,w);
+            high+=w;
+        }
+        int ans=0;
+        while (low<=high) {
+            int cap=low+(high-low)/2;
+            if(canShip(weights, cap, days)){
+                ans=cap;
+                high=cap-1;
+            }else{
+                low=cap+1;
+            }
+        }
+        return ans;
+
+    }
+    private boolean canShip(int[] weights,int cap,int days){
+            int d=1;
+            int curr=0;
+            for(int w:weights){
+                if(curr+w>cap){
+                    d++;
+                    curr=w;
+                }else{
+                    curr+=w;
+                }
+                
+            }return d<=days;
+    }
+}
