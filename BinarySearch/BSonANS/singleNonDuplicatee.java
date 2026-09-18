73a0a644ce8ca5a6045fc32e693cad73ab875040
sangram <sangramsinhpawar232@gmail.com>
2026-09-18 13:16:40 +0530

    l

---
+++ BinarySearch/BSonANS/singleNonDuplicatee.java
@@ -0,0 +1,30 @@
+package BSonANS;
+
+public class singleNonDuplicatee {
+    public int singleNonDuplicate(int[] arr) {
+        int n=arr.length;
+
+        if(n==1) return  arr[0];
+         if (arr[0] != arr[1]) return arr[0];
+
+        // Edge case: last element is the unique one
+        if (arr[n - 1] != arr[n - 2]) return arr[n - 1];
+        int low = 1, high = n - 2;
+
+        while (low<=high) {
+            int mid=(low+high)/2;
+
+            if(arr[mid]!=arr[mid+1]&& arr[mid]!=arr[mid-1]){
+                return arr[mid];
+            }
+            if ((mid % 2 == 1 && arr[mid] == arr[mid - 1]) ||
+                (mid % 2 == 0 && arr[mid] == arr[mid + 1])){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return -1;
+    }
+}
