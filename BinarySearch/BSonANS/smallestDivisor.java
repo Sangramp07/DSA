78d3718dcdb556ef75d8ba32c37f2e3042b81127
sangram <sangramsinhpawar232@gmail.com>
2026-09-19 15:04:30 +0530

    l

---
+++ BinarySearch/BSonANS/smallestDivisor.java
@@ -0,0 +1,30 @@
+package BSonANS;
+
+import java.util.Arrays;
+
+public class smallestDivisor {
+    public int smallestDivisor(int[] nums, int threshold) {
+        if(nums.length>threshold) return -1;
+
+        int low=1;
+        int high=Arrays.stream(nums).max().getAsInt();
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(sumByD(nums,mid)<=threshold){
+                high=mid-1;
+
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return low;
+    }
+    private int sumByD(int[] nums,int div){
+        int sum=0;
+        for(int num:nums){
+            sum+=Math.ceil((double)num/div);
+        }
+        return sum;
+    }
+}
