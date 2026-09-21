0b83f7497ada3223f09ee7bf53fe306f8b12d78e
sangram <sangramsinhpawar232@gmail.com>
2026-09-21 13:35:06 +0530

    l

---
+++ BinarySearch/BSonANS/maxDistancee.java
@@ -0,0 +1,39 @@
+package BSonANS;
+
+import java.util.Arrays;
+
+public class maxDistancee {
+    public int maxDistance(int[] position, int m) {
+        Arrays.sort(position);
+        int low=1;
+
+        int high=position[position.length-1]-position[0];
+        int ans=0;
+        while (low<=high) {
+            int mid=low+(high-low)/2;
+            if(canplace(position, m, mid)){
+                ans=mid;
+                low=mid+1;
+            }else{
+                high=mid-1;
+            }
+        }
+        return ans;
+    }
+    private boolean canplace(int[] pos,int m,int dist){
+        int count=1;
+        int last=pos[0];
+
+
+        for(int i=1;i<pos.length;i++){
+            if(pos[i]-last>=dist){
+                count++;
+                last=pos[i];
+            }
+            
+        }if(count>=m){
+            return true;
+        }
+        return false;
+    }
+}
